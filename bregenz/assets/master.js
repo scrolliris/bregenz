8ea06f6232c59df923411ac869a2bd5adc5e9d1f
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-10-08 01:14:59 +0900

    Fix css order

--- bregenz/assets/master.js
+++ bregenz/assets/master.js
@@ -1,10 +1,10 @@
-// global styles
-import './css/typography.styl'
-import './css/layout.styl'
-
 // vendor
 import 'open-iconic.svg'
 import 'styr.css'
+
+// global styles
+import './css/typography.styl'
+import './css/layout.styl'
 
 // application
 import './css/index.styl'
